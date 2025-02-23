1ba96a54aa9f2a60d5a5c6cc0ce84cbb125b21fd
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-02-24 01:22:38 +0530

    small changes

--- change.py
+++
@@ -1,5 +0,0 @@
-import os
-from moviepy.config import change_settings
-
-# Set the FFMPEG binary path
-change_settings({"FFMPEG_BINARY": "D:\\WorkShit\\ffmpeg-master-latest-win64-gpl-shared\\bin\\ffmpeg.exe"})
